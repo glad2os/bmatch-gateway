28410e243f47b26cc1595eb6a9862dc2b8878760
Ruslan Ibragimov <lite.console4bit@gmail.com>
2023-07-23 18:50:35 +0400

    fix findUserById

--- src/main/java/quest/dine/gateway/services/UserService.java
+++ src/main/java/quest/dine/gateway/services/UserService.java
@@ -5,6 +5,7 @@ import jakarta.validation.ValidationException;
 import jakarta.validation.Validator;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import quest.dine.gateway.enums.PermissionLevel;
@@ -15,6 +16,7 @@ import quest.dine.gateway.repository.UserRepository;
 import reactor.core.publisher.Mono;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -40,39 +42,31 @@ public class UserService {
         user.setEnabled(true);
         user.setAccountNonExpired(true);
         user.setAccountNonLocked(true);
-        user.setAccountNonLocked(true);
+        user.setCredentialsNonExpired(true);
 
-        return userRepository.findByEmail(email)
-                .hasElement()
-                .flatMap(exists -> {
-                    if (exists) {
-                        return Mono.error(new UserAlreadyExistsException("User with email " + email + " already exists"));
-                    }
+        return userRepository.findByEmail(email).hasElement().flatMap(exists -> {
+            if (exists) {
+                return Mono.error(new UserAlreadyExistsException("User with email " + email + " already exists"));
+            }
 
-                    Set<ConstraintViolation<User>> violations = validator.validate(user);
-                    if (!violations.isEmpty()) {
-                        StringBuilder errorMessage = new StringBuilder();
-                        for (ConstraintViolation<User> violation : violations) {
-                            errorMessage.append(violation.getMessage()).append("; ");
-                        }
-                        return Mono.error(new ValidationException("Validation failed: " + errorMessage));
-                    }
+            Set<ConstraintViolation<User>> violations = validator.validate(user);
 
-                    return userRepository.save(user);
-                });
+            if (!violations.isEmpty()) {
+                String errorMessage = violations.stream().map(violation -> violation.getMessage() + "; ").collect(Collectors.joining());
+                return Mono.error(new ValidationException("Validation failed: " + errorMessage));
+            }
+
+            return userRepository.save(user);
+        });
     }
 
     public Mono<UserDetails> validateUserByEmailAndPassword(String email, String password) throws BadCredentialsException {
-        return userRepository.findByEmail(email)
-                .switchIfEmpty(Mono.error(new BadCredentialsException("Invalid credentials")))
-                .filter(user -> passwordEncoder.matches(password, user.getPassword()))
-                .switchIfEmpty(Mono.error(new BadCredentialsException("Invalid credentials")));
+        return userRepository.findByEmail(email).switchIfEmpty(Mono.error(new BadCredentialsException("Invalid credentials"))).filter(user -> passwordEncoder.matches(password, user.getPassword())).switchIfEmpty(Mono.error(new BadCredentialsException("Invalid credentials")));
+    }
+
+    public Mono<User> findUserById(String userId) {
+        return userRepository.findById(Long.valueOf(userId)).switchIfEmpty(Mono.error(new UsernameNotFoundException("User not found")));
     }
 
 
-    public Mono<User> findUserById(String userId) throws BadCredentialsException {
-        return userRepository.findById(Long.valueOf(userId))
-                .switchIfEmpty(Mono.error(new BadCredentialsException("User not found")));
-
-    }
 }
